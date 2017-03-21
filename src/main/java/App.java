5d112905cee205a7254e9d86cc0cec4eba1c4fc1
ChanceMagno <chancemagno@gmail.com>
2017-03-21 15:52:12 -0700

    fix spelling typos

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,10 @@ import java.io.Console;
 public class App {
   public static void main(String[] args) {
     Console myConsole = System.console();
-    System.out.println("Welcome to Event Services, to get your estimate please start by putting in your party Size. You can select from the follwing party sizes: 20, 50, 100");
+    System.out.println("Welcome to Event Services, to get your estimate please start by putting in your party Size. You can select from the following party sizes: 20, 50, 100");
     String partySizeString = myConsole.readLine();
     int partySize = Integer.parseInt(partySizeString);
-    System.out.println("Thank you, you selected a party size of " + partySize + ". Type the number(example type(1) for just Appetiezers) representing your food choice below: 1. Appetiezers 2. Appetiezers and Meal 3. Appetiezers, Meal and Dessert");
+    System.out.println("Thank you, you selected a party size of " + partySize + ". Type the number(example type(1) for just Appetizers) representing your food choice below: 1. Appetizers 2. Appetizers and Meal 3. Appetizers, Meal and Dessert");
     String foodSelectedString = myConsole.readLine();
     int foodSelected = Integer.parseInt(foodSelectedString);
     System.out.println("great, now lets choose what type of beverages you would like available. if you do not wish to have alcoholic beverages at your event just type none. Otherwise, here is a list of our beverage packages, please type the number pertaining to the package you would like: 1. Beer 2. Beer and Wine 3. Beer, Wine and Spirits.");
@@ -46,6 +46,6 @@ public class App {
 
     Event event = new Event(partySize, foodSelected, drinkSelected, entertainmentSelected, couponAdded);
     int estimateTotal = event.addEstimateTotal(partySize, foodSelected, drinkSelected, entertainmentSelected, couponAdded);
-    System.out.println("Your estimate for your event is " + estimateTotal + " Dollars.");
+    System.out.println("Your estimate for the event is " + estimateTotal + " dollars.");
   }
 }
